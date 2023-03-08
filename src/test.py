8cd5b6f5123e07bb12b813c7ecf04d4c4973eb22
VladVons <vladvons@gmail.com>
2023-03-08 19:15:15 +0200

    MyCommit

--- src/test.py
+++ src/test.py
@@ -146,6 +146,14 @@ def Test_04():
     #Res = {Res | dict(**x) for x in L1}
     #print(Res)
 
+def Test_05():
+    import gspread
+    gc = gspread.service_account()
+
+    Url = 'https://docs.google.com/spreadsheets/d/1EIwjTitfj1_oyWS7ralnUCtq8ZH0g3DWBKq3gP4qrvo/edit#gid=782031503'
+    sht = gc.open_by_url(Url)
+
+
 
 Log.AddEcho(TEchoConsoleEx())
 Task = Test_03()
