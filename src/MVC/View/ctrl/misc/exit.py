c5c07e5615b2c2f0b97240a75c5e6e3490e126e9
VladVons <vladvons@gmail.com>
2023-03-09 20:46:25 +0200

    MyCommit

---
+++ src/MVC/View/ctrl/misc/exit.py
@@ -0,0 +1,12 @@
+# Created: 2022.03.09
+# Author:  Vladimir Vons <VladVons@gmail.com>
+# License: GNU, see LICENSE for more details
+
+import sys
+#
+from IncP.FormBase import TFormBase
+
+
+class TForm(TFormBase):
+    async def _DoRender(self):
+        sys.exit()
