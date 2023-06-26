9e2dcb82cbe9f25dd7b873ca7fec2e49608e94b3
vladvons <vladvons@gmail.com>
2023-06-26 12:11:11 +0300

    MyCommit

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@
 # Created: 28.09.2016
 # Vladimir Vons, VladVons@gmail.com
 #
-rsync -vPrlt --delete 192.168.2.13::vShops /admin/1
+#rsync -vPrlt --delete 192.168.2.13::vShops /admin/1
 
 source ./common.sh
 
