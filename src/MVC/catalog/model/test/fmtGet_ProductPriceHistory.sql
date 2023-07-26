13d18d76e8a9c968e530ac09438a613ef6e04d1c
vladvons <vladvons@gmail.com>
2023-07-26 08:25:05 +0300

    MyCommit

---
+++ src/MVC/catalog/model/test/fmtGet_ProductPriceHistory.sql
@@ -0,0 +1,38 @@
+--in: aTenantId, aDays
+select
+    --rp.id,
+    --max(hrpp.price_id),
+    rpcl.title as category,
+    rpl.title,
+    array_agg(hrpp.price order by hrpp.create_date) as price,
+    round(max(hrpp.price) / MIN(hrpp.price) * 100, 1) - 100 as rate
+from 
+    ref_product rp
+left join
+    ref_product_lang rpl on 
+    (rp.id = rpl.product_id)
+left join
+    ref_product_to_category rptc on 
+    (rp.id = rptc.product_id)
+left join
+    ref_product_category_lang rpcl on 
+    (rptc.category_id = rpcl.category_id)
+left join
+    ref_product_price rpp on 
+    (rp.id = rpp.product_id)
+left join
+    hist_ref_product_price hrpp on
+    (rpp.id = hrpp.price_id)
+where 
+    (rp.tenant_id = {aTenantId}) and
+    (hrpp.create_date > now() - interval '{aDays} day')
+group by 
+    rp.id,
+    rpcl.title,
+    rpl.title
+having
+    count(rp.id) > 1
+    --and       (max(hrpp.price) > MIN(hrpp.price) * 1.0)
+order by 
+    category,
+    title
