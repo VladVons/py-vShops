a799aa32088ccf197d9cd53e2a4c7e40e11877cf
VladVons <vladvons@gmail.com>
2023-12-02 23:40:08 +0200

    MyCommit

--- src/MVC/catalog/model/ref_product0/category/fmtGet_CategoriesProducts_LangImagePrice.sql
+++ src/MVC/catalog/model/ref_product0/category/fmtGet_CategoriesProducts_LangImagePrice.sql
@@ -3,19 +3,16 @@ with
 wt1 as (
     select
         count(*) over() as total,
-        rptc.category_id as category0_id,
-        rptc.product_id as product0_id,
+        rptc0.category_id as category0_id,
+        rpcl0.title as category0_title,
         rp.id,
-        rp.idt as product_idt,
         rp.tenant_id,
         rt.title as tenant_title,
         rpl.title,
-        rpl.title as title0,
-        rpcl.title as category0_title,
         (
             select rpi.image
             from ref_product0_image rpi
-            where (rpi.product_id = rptc.product_id and rpi.enabled)
+            where (rpi.product_id = rptc0.product_id and rpi.enabled)
             order by rpi.sort_order
             limit 1
         ) as image0,
@@ -38,21 +35,21 @@ wt1 as (
             limit 1
         ) as price
     from
-        ref_product0_to_category rptc
+        ref_product0_to_category rptc0
+    left join
+        ref_product0_category_lang rpcl0
+        on (rptc0.category_id = rpcl0.category_id and rpcl0.lang_id = {{aLangId}})
     left join
         ref_product rp on
-        (rptc.product_id = rp.product0_id)
+        (rptc0.product_id = rp.product0_id)
     left join
         ref_product_lang rpl
         on (rp.id = rpl.product_id and rpl.lang_id = {{aLangId}})
     left join
-        ref_product0_category_lang rpcl
-        on (rptc.category_id = rpcl.category_id and rpcl.lang_id = {{aLangId}})
-    left join
         ref_tenant rt
         on (rp.tenant_id = rt.id)
     where
-         (rptc.category_id in ({{CategoryIds}})) and
+         (rptc0.category_id in ({{CategoryIds}})) and
          (rp.enabled) and (rp.product0_id is not null) and (rp.product0_skip is null)
     order by
         {{aOrder}}
@@ -66,11 +63,10 @@ select
     wt1.category0_id as category_id,
     wt1.category0_title as category_title,
     wt1.id as product_id,
-    wt1.product_idt,
     wt1.tenant_id,
     wt1.tenant_title,
     wt1.price::float,
-    coalesce(wt1.title, wt1.title0) as product_title,
+    wt1.title as product_title,
     coalesce(wt1.image, wt1.image0) as image
 from
     wt1
--- src/MVC/catalog/model/ref_product0/product/fmtGet_ProductsRnd_LangImagePrice.sql
+++ src/MVC/catalog/model/ref_product0/product/fmtGet_ProductsRnd_LangImagePrice.sql
@@ -2,18 +2,16 @@
 with
 wt1 as (
     select
-        rptc.category_id as category0_id,
-        rptc.product_id as product_id0,
+        rptc0.category_id as category0_id,
+        rpcl0.title as category0_title,
         rp.id,
-        rp.idt as product_idt,
         rp.tenant_id,
         rt.title as tenant_title,
         rpl.title,
-        rpl.title as title0,
         (
             select rpi.image
             from ref_product0_image rpi
-            where (rpi.product_id = rptc.product_id and rpi.enabled)
+            where (rpi.product_id = rptc0.product_id and rpi.enabled)
             order by rpi.sort_order
             limit 1
         ) as image0,
@@ -36,13 +34,16 @@ wt1 as (
             limit 1
         ) as price
     from
-        ref_product0_to_category rptc
+        ref_product0_to_category rptc0
+    left join
+        ref_product0_category_lang rpcl0
+        on (rptc0.category_id = rpcl0.category_id and rpcl0.lang_id = {{aLangId}})
     left join
         ref_product rp on
-        (rptc.product_id = rp.product0_id)
+        (rptc0.product_id = rp.product0_id)
     left join
-        ref_product0_lang rpl
-        on (rptc.product_id = rpl.product_id and rpl.lang_id = {{aLangId}})
+        ref_product_lang rpl
+        on (rptc0.product_id = rpl.product_id and rpl.lang_id = {{aLangId}})
     left join
         ref_tenant rt
         on (rp.tenant_id = rt.id)
@@ -52,57 +53,15 @@ wt1 as (
         random()
     limit
         {{aLimit}}
-),
-wt2 as (
-    with recursive wrpc as (
-        select
-            rpc.id,
-            rpc.parent_id,
-            array[rpc.id] as path_id
-        from
-            ref_product0_category rpc
-        where
-            (rpc.enabled) and
-            (rpc.id in (select category0_id from wt1))
-
-        union all
-
-        select
-            wrpc.id,
-            rpc.parent_id,
-            array[rpc.id] || wrpc.path_id
-        from
-            ref_product0_category rpc
-        join
-            wrpc on
-            (wrpc.parent_id = rpc.id)
-        where
-            (rpc.enabled)
-    )
-    select
-        wrpc.id,
-        path_id
-    from
-        wrpc
-    where
-        parent_id = 0
 )
 select
     wt1.category0_id as category_id,
+    wt1.category0_title as category_title,
     wt1.id as product_id,
-    wt1.product_idt,
     wt1.tenant_id,
     wt1.tenant_title,
     wt1.price::float,
-    rpcl.title as category_title,
-    coalesce(wt1.title, wt1.title0) as product_title,
-    coalesce(wt1.image, wt1.image0) as image,
-    wt2.path_id
+    wt1.title as product_title,
+    coalesce(wt1.image, wt1.image0) as image
 from
     wt1
-left join 
-    wt2 on 
-    wt1.category0_id = wt2.id
-left join 
-    ref_product0_category_lang rpcl on 
-    wt1.category0_id = rpcl.category_id 
