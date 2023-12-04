e65d9a6ac98cebdebf0ab16dad0bada4edad18bd
VladVons <vladvons@gmail.com>
2023-12-04 06:42:04 +0200

    autocomplete

--- src/MVC/catalog/view/default/assets/js/search1.js
+++ src/MVC/catalog/view/default/assets/js/search1.js
@@ -8,44 +8,85 @@ as function namespace
 
 
 function searchNavbar() {
+    let currentFocus = 0
     let timeout = null
     const elSearchInput = document.getElementById('viSearchInput')
     const elSearchSuggest = document.getElementById('viSearchSuggest')
 
+    elSearchInput.addEventListener('keydown', function(aEvent) {
+        let x = elSearchSuggest.getElementsByTagName('div')
+        if (x.length == 0)
+            return
+
+        if (aEvent.key == 'ArrowDown') {
+            currentFocus++
+            setActive(x)
+        } else if (aEvent.key == 'ArrowUp') {
+            currentFocus--
+            setActive(x)
+        } else if (aEvent.key == 'Enter') {
+            aEvent.preventDefault()
+            if (currentFocus >= 0) {
+                x[currentFocus].click()
+            }
+        } else {
+            currentFocus = -1
+        }
+    })
+
     elSearchInput.addEventListener('input', function(aEvent) {
-        const value = this.value.trim()
-        if (value.length > 0) {
-            clearTimeout(timeout)
-            timeout = setTimeout(() => {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+            const value = this.value.trim()
+            if (value.length > 0) {
                 const url = gData.getValue('/href/search_ajax')
                 postJson(url, {'method': 'ajax', 'q': value})
                     .then(data => {
                         displayResult(aEvent, data)
                     })
-            }, 500)
-        } else {
-            elSearchSuggest.innerHTML = ''
-        }
+            } else {
+                elSearchSuggest.innerHTML = ''
+            }
+        }, 500)
     })
 
     function displayResult(aEvent, aResults) {
         elSearchSuggest.innerHTML = ''
 
         if (aResults && aResults.length > 0) {
-            //const keys = aEvent.target.value.split(' ').filter(key => key !== '')
+            const keys = aEvent.target.value.split(' ').filter(key => key !== '')
             //const pattern = new RegExp(`\\b(${keys.join('|')})\\b`, 'gi')
+            const pattern = new RegExp(`(${keys.join('|')})`, 'gi')
 
             aResults.forEach(function(aResult) {
-                let option = document.createElement('option')
-                option.value = aResult
-                //option.value = aResult.replace(pattern, '<b>$1</b>')
-                elSearchSuggest.appendChild(option)
+                const elDiv = document.createElement('div')
+                elDiv.innerHTML = aResult.replace(pattern, '<b>$1</b>') + `<input type='hidden' value='${aResult}'>`
+                elDiv.addEventListener("click", function(e) {
+                    const value = this.getElementsByTagName("input")[0].value
+                    aEvent.target.value = value
+                    elSearchSuggest.innerHTML = ''
+                    window.location.href = '?route=product0/search&q=' + encodeURIComponent(value)
+                })
+                elSearchSuggest.appendChild(elDiv)
             })
+        }
+    }
 
-            const selectedValue = aEvent.target.value
-            if (aResults.includes(selectedValue)) {
-                window.location.href = '?route=product0/search&q=' + encodeURIComponent(selectedValue)
-            }
+    function setActive(x) {
+        delActive(x)
+
+        if (currentFocus >= x.length) 
+            currentFocus = 0
+            
+        if (currentFocus < 0) 
+            currentFocus = (x.length - 1)
+
+        x[currentFocus].classList.add('autocomplete-active')
+    }
+
+    function delActive(x) {
+        for (let i = 0; i < x.length; i++) {
+            x[i].classList.remove('autocomplete-active');
         }
     }
 }
