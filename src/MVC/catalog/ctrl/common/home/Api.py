a5e02b943dc1f152a2c56aefd3f656305d02b14f
VladVons <vladvons@gmail.com>
2024-02-29 11:51:00 +0200

    MyCommit

---
+++ src/MVC/catalog/ctrl/common/home/Api.py
@@ -0,0 +1,7 @@
+# Created: 2024.02.29
+# Author: Vladimir Vons <VladVons@gmail.com>
+# License: GNU, see LICENSE for more details
+
+
+async def Main(self, aData: dict = None) -> dict:
+    pass
---
+++ src/Test/Schama.py
@@ -0,0 +1,6 @@
+from Inc.Scheme.Scheme import TSoupScheme
+
+def Test1():
+    Scheme = TSoupScheme()
+
+Test1()
