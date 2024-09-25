e3c97af013a8e1c169b66849531f8d9956679575
VladVons <vladvons@gmail.com>
2024-09-26 02:00:18 +0300

    MyCommit

--- src/IncP/Log.py
+++ src/IncP/Log.py
@@ -3,25 +3,7 @@
 # License: GNU, see LICENSE for more details
 
 
-import asyncio
-#
-from Inc.Log import TLog, TEcho
-
-
-class TEchoDb(TEcho):
-    def __init__(self, aDb):
-        super().__init__(aType = 'ex')
-        self.Db = aDb
-        self.Fmt = ['aL', 'aT', 'aM', 'aD', 'aE']
-
-    async def _Write(self, aMsg: str):
-        #await self.Db.AddLog(1, aMsg)
-        #ToDo
-        pass
-
-    def Write(self, aArgs: dict):
-        if (aArgs.get('aL') <= self.Level) and (aArgs.get('aT') in self.Type):
-            Msg = self._Format(aArgs)
-            asyncio.create_task(self._Write(Msg))
+from Inc.Log import TLog
+from Inc.Log.EchoDb import TEchoDb
 
 Log = TLog()
